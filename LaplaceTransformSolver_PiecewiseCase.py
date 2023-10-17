544004fff4cdc556e3f172d7ed4f75795139a028
bdroidd <83428884+bdroidd@users.noreply.github.com>
2023-10-17 18:47:25 +0300

    Add files via upload

---
+++ LaplaceTransformSolver_PiecewiseCase.py
@@ -0,0 +1,17 @@
+%
+%   LaplaceTransformSolver_PiecewiseCase.
+%
+%   The following program applies the LaPlace transform method to a set of 
+%   three specific piecewise functions to determine its solution.
+%______________________________________________________________________________
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+syms t
+
+f = piecewise(0 < t < 1, 1, 1 < t < 2, -2, 2 < t, t^2);
+
+F = laplace(f);
+
+disp(F)
